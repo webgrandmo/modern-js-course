54162e0839dd95e4cf0c3b5e7b3b36b6a7ac5917
Andriy Trofimov <andriy.trofimov@valtech.com>
2023-06-21 21:21:36 +0300

    Chuck Norris Joke Generator

---
+++ aynchronus-js/joke-generator/index.js
@@ -0,0 +1,19 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+const getJoke = () => {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+	xhr.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			const data = JSON.parse(this.responseText);
+			jokeEl.innerHTML = data.value;
+		} else {
+			jokeEl.innerHTML = `<p style="color: red">Sorry, something went wrong. <br>${this.statusText} ${this.status}</p>`;
+		}
+	};
+	xhr.send();
+};
+
+jokeBtn.addEventListener('click', getJoke);
