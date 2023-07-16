68807697af397889b01dfa05fc0c7c8e5716b870
Andriy Trofimov <andriy.trofimov@valtech.com>
2023-07-16 23:14:38 +0300

    requestAnimationFrame

---
+++ web-api/request-animation-frame/index.js
@@ -0,0 +1,23 @@
+const image = document.querySelector('img');
+let start;
+let done = false;
+
+function step(timestamp) {
+	if (start === undefined) {
+		start = timestamp;
+	}
+
+	const elapsed = timestamp - start;
+	image.style.transform = `translateX(${elapsed / 10}px) rotate(${elapsed / 10}deg)`;
+
+	if (elapsed > 5000) {
+		done = true;
+	}
+
+	if (done) {
+		return;
+	}
+	requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
